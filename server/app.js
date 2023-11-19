dfb4e0a1f44b698c0c99cc2b10b439b2e14b0c97
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-11-19 23:46:15 +0530

    update routes

--- server/app.js
+++ server/app.js
@@ -5,6 +5,13 @@ require('./config/connectDB');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+// app.get('/', (req, res) => {
+//     res.send({message: "Welcome to Home Page"});
+// })
+app.get('/', (req, res) => {
+    res.send('API is Working');
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`)
 });
